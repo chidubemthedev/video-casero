4cdcc15cc73aaaee2064de0014fd09ca9948bfa2
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2025-06-27 22:08:09 +0100

    remove unused imports

--- src/components/shared/dropdown-list.tsx
+++ src/components/shared/dropdown-list.tsx
@@ -2,8 +2,6 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { ChevronRight, Menu } from "lucide-react";
