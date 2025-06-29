20f680872493b445421b37c0a40dfb36b64cd80c
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2025-06-29 06:56:29 +0100

    added video card

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,58 @@
 import Header from "@/components/shared/header";
+import VideoCard from "@/components/shared/video-card";
+
+const VIDEOS = [
+  {
+    id: "1",
+    title: "Screen record - 001",
+    thumbnail:
+      "https://images.unsplash.com/photo-1749741355867-8d40976f2bfb?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8",
+    createdAt: "2025-05-01 06:00:00",
+    userImage:
+      "https://i.pinimg.com/originals/27/ff/79/27ff79f41b15e35d5b8cc7a50ad96cb4.jpg",
+    userName: "Jason Smith",
+    views: 1000,
+    visibility: "public",
+    duration: 156,
+  },
+  {
+    id: "2",
+    title: "Screen record - 002",
+    thumbnail:
+      "https://images.unsplash.com/photo-1751104486292-e35ffb54b336?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8",
+    createdAt: "2025-05-09 06:00:00",
+    userImage:
+      "https://i.pinimg.com/originals/27/ff/79/27ff79f41b15e35d5b8cc7a50ad96cb4.jpg",
+    userName: "Nens Moriah",
+    views: 356,
+    visibility: "public",
+    duration: 1056,
+  },
+  {
+    id: "3",
+    title: "Screen record - 003",
+    thumbnail:
+      "https://images.unsplash.com/photo-1750544684761-cc028988a69e?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8",
+    createdAt: "2025-05-09 06:04:00",
+    userImage:
+      "https://i.pinimg.com/originals/27/ff/79/27ff79f41b15e35d5b8cc7a50ad96cb4.jpg",
+    userName: "Nens Moriah",
+    views: 56,
+    visibility: "public",
+    duration: 656,
+  },
+];
 
 const Home = () => {
   return (
     <>
       <Header title="All Videos" subHeader="Public Library" />
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+        {VIDEOS.map((video) => (
+          <VideoCard key={video.id} {...video} />
+        ))}
+      </div>
     </>
   );
 };
---
+++ src/components/shared/video-card.tsx
@@ -0,0 +1,98 @@
+"use client";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Eye, Link2 } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+type Props = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  createdAt: string;
+  userImage: string;
+  userName: string;
+  views: number;
+  visibility: string;
+  duration: number;
+};
+
+const VideoCard = ({
+  id,
+  title,
+  thumbnail,
+  createdAt,
+  userImage,
+  userName,
+  views,
+  visibility,
+  duration,
+}: Props) => {
+  return (
+    <Link href={`/profile/${id}`}>
+      <Card className="pt-0">
+        <CardHeader className="relative px-0">
+          <div className="rounded-2xl overflow-hidden">
+            <Image
+              src={thumbnail}
+              alt={title}
+              width={300}
+              height={200}
+              className="w-full h-[200px] object-cover hover:scale-125 transition-all duration-300"
+            />
+          </div>
+          <Button
+            onClick={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              alert("hello");
+            }}
+            className="absolute top-2 right-2"
+            size={"icon"}
+          >
+            <Link2 />
+          </Button>
+          <Button className="absolute bottom-4 right-2 bg-gray-600" size={"sm"}>
+            {Math.ceil(duration / 60)} min
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-2">
+              <Avatar>
+                <AvatarImage src={userImage} />
+                <AvatarFallback>{userName.charAt(0)}</AvatarFallback>
+              </Avatar>
+              <div>
+                <CardTitle className="text-sm">{userName}</CardTitle>
+                <CardDescription className="text-xs">
+                  {visibility}
+                </CardDescription>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-1">
+              <Eye /> {views}
+            </div>
+          </div>
+
+          <div className="mt-4 flex items-center gap-4">
+            <CardTitle>{title}</CardTitle>
+            <CardDescription>
+              {new Date(createdAt).toLocaleDateString()}
+            </CardDescription>
+          </div>
+        </CardContent>
+      </Card>
+    </Link>
+  );
+};
+
+export default VideoCard;
